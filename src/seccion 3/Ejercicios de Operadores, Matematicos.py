ebfb7a52e0c2360f3a6b7ce399f4fc6df4faec7c
Kevin <kevinmg253@gmail.com>
2026-07-27 08:44:54 -0500

    primer commit

---
+++ src/seccion 3/Ejercicios de Operadores, Matematicos.py
@@ -0,0 +1,64 @@
+#ejercicio 1
+#el resultado debe ser 11 y que primero se hace la multiplicacion y despues se hace la suma
+print(5 + 3 * 2)
+
+#ejercicio 2 
+#el resultado es 16 porqque primero se hace la multiplicacion y despues la division y de ultimo la suma
+print(8 / 2 + 4 * 3)
+
+#ejercicio 3 
+#el resultado es 15 porque primero se realiza lo que esta dentro de los parentesis y despues la multiplicacion y de ultimo la resta
+print((7 + 3) * 2 - 5)
+
+#ejercicio 4
+#el resultado es 12 porque primero e hace la multiplicacion y despues la resta y de ultimo la suma
+
+print(10 - 4 + 2 * 3)
+
+#ejercicio 5
+# el resultado es 21.0 porque primero se hacen los parentecis luego la multiplicacion y despues la resta
+
+print((10 / 2) * (3 + 2) - 4)
+
+#ejercicio 6
+# el resultado es 11 porque primero se hacen los parantesis luego la multiplicacion y despues de la suma por eso da 11
+
+print(2 + 3 * (4 - 1))
+
+#ejercicio 7
+# el resultado es 40 porque primero se hace la potencia y despues la multiplicacion
+print(5 * 2 ** 3)
+
+#ejercicio 8
+# el resultado es 7.0 porque primero se hace la potencia y despues la division y de ultimo la suma
+print(6 + 4 / 2 ** 2)
+
+#ejercicio 9
+#el resultado es 11 porque primero se hace la multiplicacion y luego la suma del residuo
+
+print(10 % 3 + 2 * 5)
+
+#ejercicio 10
+# el resultado es 90 porque primero se hace la potencia y despues la multiplicacion y de ultimo la suma
+print((8 + 2 ) * 3 ** 2)
+
+#ejercicio 11
+#el resultado es 11.0 porque primero se hace los parentesis y luego la multiplicacion, luego la division y de ultimo la suma
+print(7 + 2 * (3 + 5) / 4)
+
+#ejercicio 12
+#el resultado es 16.0 primero se hacen las potencias, luego la multiplicacion y de ultimo la division 
+
+print(2 ** 3 * 4 / 2)
+
+#ejercicio 13
+#el resultado es 12 porque primero se hace la potencia y luego la resta y de ultimo la suma
+print(9 - 6 + 3 ** 2)
+
+#ejercicio 14
+#el resultado es 34 porque primero sse hace los parentesis luego la potencia luego la multiplicacion y de ultimo la suma
+print((7 - 2) * 5 + 3 ** 2)
+
+#ejercicio 15
+#el resultado es 5.0 porque primero se hace la potencia y luego la multiplicacion y de ultimo la suma
+print(4 * 2 ** 3 / 8 + 1)
